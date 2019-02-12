54b0708d4c4390a941d46e87f0942f48e2b81d2d
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-02-12 14:26:59 -0600

    Added functionality to display the board in HTML with button inputs.

--- code/minesweeper.js
+++ code/minesweeper.js
@@ -7,3 +7,18 @@ for (let i=0;i<width;i++){
         board[i][j] = 0;
 	}
 }
+
+displayBoard(board);
+
+function displayBoard(board){
+	let count = 0;
+	for (let i = 0; i<width; i++){
+		for (let j = 0; j<height;j++){
+		document.write("<input type='button' id='abc' value='  ' onclick='reveal(id)' oncontextmenu='flag(id);return false;'></input>");
+		document.getElementById("abc").value=board[i][j];
+		document.getElementById("abc").id= count;
+		count++;
+		}
+		document.write("<br>");
+	}
+}
