d6a744d11e007d205852768b48ecc37c38e1b147
Jason <JasonDing@gmail.com>
2019-02-15 00:58:12 -0600

    works version

--- code/minesweeper.js
+++ code/minesweeper.js
@@ -42,7 +42,6 @@ class Board{
 	width(w){
 		this.width =w;
 	}
-	
 	initboard(){
 		for (let i=0;i<this.width;i++){
 			this.board[i] = [];
@@ -53,7 +52,6 @@ class Board{
 			}
 		}
 	}
-
 	initMines(nmines){
 		if(nmines !=0){
 			let x = Math.floor(Math.random()*this.width);
@@ -65,18 +63,16 @@ class Board{
 			this.initMines(nmines);
 		}
 	}
-
 	displayBoard(){
 		console.table(this.board);
 		for (let i = 0; i<this.width; i++){
 			for (let j = 0; j<this.height;j++){
 			this.nmines_array[i][j] = this.countMines(i,j);
 			}
+			//console.log("");
 		}
 		console.table(this.nmines_array);
-		console.log(this.mines);
 	}
-
 	countMines(x,y){
 	let nx,ny;
 	let nmines=0;
@@ -94,7 +90,7 @@ class Board{
 		}
 	}
 	if(this.board[x][y] == 1){
-		return("B");
+		return(9);
 	}
 	else{
 		return(nmines);
@@ -102,8 +98,12 @@ class Board{
 	}
 }
 
+let gameBoard = new Board("",9,9,10);
+gameBoard.displayBoard();
+console.log(gameBoard.mines);
+
 class UIclass{
-	constructor(gameBoard){
+	constructor(){
 		
 	}
 
@@ -134,157 +134,95 @@ class UIclass{
 								reveal(tc);
 								return;
 							}
-							//else
-								//rcount++;
 						}
 					}
 				}
 			}
 		}
 	}
-// 	CheckComplete(){
-// 		let over = 0;
-// 		for (let i = 0; i<this.width; i++){
-// 			for (let j = 0; j<this.height;j++){
-// 				if ()
-// 			}
-// 		}
-// 	}
-// 	Flag(c){
-// 		switch(document.getElementById(c).value){
-// 			case 'F': 
-// 						document.getElementById(c).value = '?';
-// 						break;
-// 			case '?': 
-// 						document.getElementById(c).value = '  ';
-// 						break;
-// 			default:
-// 						document.getElementById(c).value = 'F';
-// 						break;
-// 		}
-// 	}
-}
-let gameBoard = new Board("",9,9,20);
-let map = new UIclass(gameBoard);
-map.ShowBoard(); 
-
-// gameBoard.displayBoard();
-// console.log(gameBoard.mines);
-
-/*let board = [];
-let rcount=0;
-let width = 9, height = 9;
-let mines = 10;
-for (let i=0;i<width;i++){
-	board[i] = [];
-    for (let j = 0;j<height;j++){
-        board[i][j] = 0;
-	}
 }
 
-setMines(mines);
-
-function setMines(nmines){
-	if(nmines !=0){
-		let x = Math.floor(Math.random()*width);
-		let y = Math.floor(Math.random()*height);
-		if(!(board[x][y]==1)){
-			board[x][y] = 1;
-			nmines--;
+class Graphics{
+	constructor(){
+		this.comp = [];
+		this.canvas = document.createElement("canvas");
+		this.canvas.width = gameBoard.width*50;
+		this.canvas.height = gameBoard.height*50;
+		this.canvas.style = "border: 1px solid #d3d3d3";
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.draw();		
 		}
-		setMines(nmines);
+	component(x,y,t=""){
+		this.width = 50;
+		this.height = 50;
+		this.x = x;
+		this.y =y;
+		this.context.strokeRect(this.x, this.y, this.width, this.height);
+		this.context.fillText(t,x+25,y+25);
 	}
-}
-
-displayBoard(board);
-
-function displayBoard(board){
-	for (let i = 0; i<width; i++){
-		for (let j = 0; j<height;j++){
-		document.write("<input type='button' id='abc' value='  ' onclick='reveal(id)' oncontextmenu='flag(id); return false;'></input>");
-		document.getElementById("abc").value=board[i][j];
-		document.getElementById("abc").id= "" + i + j;
-		}
-		document.fwrite("<br>");
-	}
-}
-
-function countMines(x,y,c){
-	let nx,ny;
-	let nmines=0;
-	if(board[x][y]==0){
-		for(let i=-1;i<=1;i++){
-			for(let j=-1;j<=1;j++){
-				nx = x + i;
-				ny = y +j;
-				if(!(i == 0 && j == 0)){
-					if(nx >= 0 && nx <width && ny >= 0 && ny < height)
-						if(board[nx][ny]==1)
-						nmines++;
-				}
+	draw(){
+		for(let i =0; i<gameBoard.width;i++){
+			this.comp[i] = [];
+			for(let j =0; j<gameBoard.height;j++){
+				this.comp[i][j] = this.component(i*50,j*50);
 			}
 		}
 	}
-	if(board[x][y] == 1){
-		document.getElementById(c).value = 'B';
+}
+
+let gfx = new Graphics();
+
+gfx.canvas.addEventListener('click',clickHandler);
+gfx.canvas.addEventListener('contextmenu',clickHandler);
+
+function clickHandler(e){
+	e.preventDefault();
+	let tc ="";
+	let clickX = e.pageX - gfx.canvas.offsetLeft;
+	let clickY = e.pageY - gfx.canvas.offsetTop;
+	let x = Math.floor(clickX/50);
+	let y = Math.floor(clickY/50);
+	let rightClick = false;
+	if(e.which){
+		rightClick = (e.which === 3);
+	}
+	if(!rightClick){
+		if((gameBoard.nmines_array[y][x]!=0)&&(gameBoard.nmines_array[y][x]!=9)){
+			tc =  "" + gameBoard.nmines_array[y][x];
+			gfx.component(x*50,y*50,"");
+			gfx.component(x*50,y*50,tc);
+		}
+		if(gameBoard.nmines_array[y][x]==0){
+			WhenChick_0(y,x);
+		}
+		if(gameBoard.nmines_array[y][x]==9){
+			
+		}
 	}
 	else{
-		if(nmines !=0){
-			document.getElementById(c).value = nmines;
-		}
-		else{
-			document.getElementById(c).value = '  ';
-		}
+		tc = "F";
+		gfx.component(x*50,y*50,tc);
 	}
 }
 
-function flag(c){
-	switch(document.getElementById(c).value){
-		case 'F': 
-					document.getElementById(c).value = '?';
-					break;
-		case '?': 
-					document.getElementById(c).value = '  ';
-					break;
-		default:
-					document.getElementById(c).value = 'F';
-					break;
-	}
-}
-
-function reveal(c){
-	if(rcount>=8){
-		rcount = 0;
-		return;
-	}
-	rcount++;
-	let x = c[0];
-	let y = c[1];
-	let ix = parseInt(x,10);
-	let iy = parseInt(y,10);
-	countMines(ix,iy,c);
-	document.getElementById(c).disabled = true;
-	if(board[ix][iy] == 0){
-		let nx=0;
-		let ny=0;
-		let tc;
-		for (let i = -1; i<=1;i++){
-			for (let j = -1; j<=1; j++){
-				nx = ix + i;
-				ny = iy +j;
-				if(!(i == 0 && j == 0)){
-					if(nx >= 0 && nx <width && ny >= 0 && ny < height){
-						tc = "" + nx + ny;
-						if(board[nx][ny]==0 && !document.getElementById(tc).disabled){
-							reveal(tc);
-							return;
-						}
-						//else
-							//rcount++;
-					}
+function WhenChick_0(y,x){
+	if(gameBoard.nmines_array[y][x]==0){
+		tc =  "" + gameBoard.nmines_array[y][x];
+		gfx.component(x*50,y*50,"");
+		gfx.component(x*50,y*50,tc);
+		gameBoard.nmines_array[y][x] = gameBoard.nmines_array[y][x]+10;
+		for (let i =y-1; i <= y+1; i++){
+			for (let j = x-1; j <= x+1; j++){
+				if (i>=0&&i<9&&j>=0&&j<9){				
+					WhenChick_0(i,j);
 				}
 			}
 		}
 	}
+	else if(gameBoard.nmines_array[y][x]<=8){
+		tc =  "" + gameBoard.nmines_array[y][x];
+		gfx.component(x*50,y*50,"");
+		gfx.component(x*50,y*50,tc);
+	}
 }
-*/
