f67da5d59138874991cc9e405e480113d4f4bc7b
Jason <JasonDing@gmail.com>
2019-02-16 16:45:30 -0600

    checkComplete() update

--- code/minesweeper.js
+++ code/minesweeper.js
@@ -1,8 +1,18 @@
+// Implement a checkComplete function in class UI
+// Implement correct/incorrect mine flagged functionality
+// Implement Number of mines remaining.
+// Improve the graphics a bit.
 
+
+
+// This is a class for board, the game board is initiallized here with the given width, height,
+// and number of mines
 class Board{
-	constructor (level,h,w,m){
-		this.board = [];
-		this.nmines_array = [];
+	//default constructor of the board class.
+	constructor (level,h,w,m){		
+		this.board = [];			// Initiallizing the board array;
+		this.nmines_array = [];		// Initiallizing the number of mines surrounding array.
+		// Switches between the size of boards;
 		switch(level){
 			case "b":
 					this.height(3);
@@ -10,6 +20,7 @@ class Board{
 					this.mines(4);
 					this.initboard();
 					this.initMines(this.mines);
+					this.initNmines();
 					break;
 			case "e":
 					this.height(9);
@@ -17,6 +28,7 @@ class Board{
 					this.mines(10);
 					this.initboard();
 					this.initMines(this.mines);
+					this.initNmines();
 					break;
 			case "":
 					this.height(h);
@@ -24,6 +36,7 @@ class Board{
 					this.mines(m);
 					this.initboard();
 					this.initMines(this.mines);
+					this.initNmines();
 					break;
 			default:
 					this.height(5);
@@ -31,18 +44,23 @@ class Board{
 					this.mines(6);
 					this.initboard();
 					this.initMines(this.mines);
+					this.initNmines();
 					break;
 		}
 	}
+	// sets the number of mines.
 	mines(n){
 		this.mines = n;
 	}
+	//sets the height of the board;
 	height(h){
 		this.height = h;
 	}
+	// sets the width of the board;
 	width(w){
 		this.width =w;
 	}
+	//Initiallizes the baord array and nmines array with default values, no mines all 0's.
 	initboard(){
 		for (let i=0;i<this.width;i++){
 			this.board[i] = [];
@@ -53,26 +71,29 @@ class Board{
 			}
 		}
 	}
+	//Sets the given number of mines in the board, i.e. initiallize the board array with 1;
 	initMines(nmines){
-		if(nmines !=0){
-			let x = Math.floor(Math.random()*this.width);
+		if(nmines !=0){		// Break condition, breaks when there are no remaining mines to set.
+			let x = Math.floor(Math.random()*this.width); //Get a random x coordinate, multiplying width/height with a random [0,1], and then floor gives us an index in range.
 			let y = Math.floor(Math.random()*this.height);
-			if(!(this.board[x][y]==1)){
+			if(!(this.board[x][y]==1)){		//Checking that we are not placing a mine where one already exists.
 				this.board[x][y] = 1;
 				nmines--;
 			}
-			this.initMines(nmines);
+			this.initMines(nmines); 	// Recursive call to initiallize all the mines.
 		}
 	}
-	displayBoard(){
-		console.table(this.board);
+
+	//Initiallizes the number of mines arround array with what countMines(x,y) returns and returns a number for nmines_array.
+	initNmines(){
 		for (let i = 0; i<this.width; i++){
 			for (let j = 0; j<this.height;j++){
 			this.nmines_array[i][j] = this.countMines(i,j);
 			}
 		}
-		console.table(this.nmines_array);
 	}
+
+	// Counts the number of mines surrounding the tile.
 	countMines(x,y){
 	let nx,ny;
 	let nmines=0;
@@ -98,11 +119,63 @@ class Board{
 	}
 }
 
-class UIclass{
-	constructor(){
-		
-	}
+	// Initiallizing a Board Object, with parameters from the prompt.
+	let gameBoard = new Board("",prompt("Enter height"),prompt("Enter width"),prompt("Enter number of mines"));
+	console.table(gameBoard.nmines_array);
+	console.log(gameBoard.mines);
 
+
+class UI{
+	constructor(){
+		this.revealed = [];
+		this.n_array = [];
+		this.init();
+	}
+	init(){
+		for(let i=0;i<gameBoard.width;i++){
+			this.revealed [i] = [];
+			this.n_array [i] = [];
+			for(let j =0; j<gameBoard.height; j++){
+				this.revealed[i][j]=false;
+				this.n_array[i][j] = gameBoard.nmines_array[i][j];
+			}
+		}
+	}
+	clickCheck(x,y){
+		let tc = "";
+		this.revealed[x][y] = true;
+		gfx.context.strokeStyle = "red";
+		if(this.n_array[x][y]==0){
+			tc =  "" + this.n_array[x][y];
+			gfx.component(x*50,y*50,tc);
+			this.n_array[x][y] = this.n_array[x][y]+10;
+			for (let i =x-1; i <= x+1; i++){
+				for (let j = y-1; j <= y+1; j++){
+					if (i>=0&&i<gameBoard.width&&j>=0&&j<gameBoard.height){	
+						//if(!(i==0 && j ==0))			
+							this.clickCheck(i,j);
+					}
+				}
+			}
+		}
+		else if(this.n_array[x][y]<=8){
+			tc =  "" + this.n_array[x][y];
+			gfx.component(x*50,y*50,tc);
+			this.n_array[x][y] = this.n_array[x][y]+10;
+		}
+	}
+	
+	CheckWin(){
+		let over = true;
+		for (let i = 0; i < gameBoard.width; i++){
+			for (let j = 0; j < gameBoard.height; j++){
+				if (this.n_array[i][j] < 9){				
+					over = false;
+				}
+			}
+		}
+		return over;
+	}
 }
 
 class Graphics{
@@ -111,7 +184,7 @@ class Graphics{
 		this.canvas = document.createElement("canvas");
 		this.canvas.width = gameBoard.width*50;
 		this.canvas.height = gameBoard.height*50;
-		this.canvas.style = "border: 1px solid #d3d3d3";
+		//this.canvas.style = "border: 1px solid #d3d3d3";
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.draw();		
@@ -134,18 +207,9 @@ class Graphics{
 	}
 }
 
-var height;
-var wide;
-var minenumber;
-height = prompt("Enter a height");
-wide=prompt("Enter a wide");
-minenumber=prompt("Enter number of mines");
-
-let gameBoard = new Board("",height,wide,minenumber);
-gameBoard.displayBoard();
-console.log(gameBoard.mines);
-
+let ui = new UI();
 let gfx = new Graphics();
+
 gfx.canvas.addEventListener('click',clickHandler);
 gfx.canvas.addEventListener('contextmenu',clickHandler);
 
@@ -160,72 +224,60 @@ function clickHandler(e){
 	if(e.which){
 		rightClick = (e.which === 3);
 	}
+	if(!ui.revealed[x][y])
 	if(!rightClick){
-		if((gameBoard.nmines_array[y][x]>0)&&(gameBoard.nmines_array[y][x]<9)){
-			tc =  "" + gameBoard.nmines_array[y][x];
-			gfx.component(x*50,y*50,"");
+		ui.revealed[x][y] = true;
+		gfx.context.strokeStyle = "red";
+		if((ui.n_array[x][y]>0)&&(ui.n_array[x][y]<9)){
+			tc =  "" + ui.n_array[x][y];
 			gfx.component(x*50,y*50,tc);
-			gameBoard.nmines_array[y][x] = gameBoard.nmines_array[y][x]+10;
+			ui.n_array[x][y] = ui.n_array[x][y]+10;
 		}
-		if(gameBoard.nmines_array[y][x]==0){
-			WhenChick_0(y,x);
+		if(ui.n_array[x][y]==0){
+			ui.clickCheck(x,y);
 		}
-		if(gameBoard.nmines_array[y][x]==9){
+		if(ui.n_array[x][y]==9){
 			alert("Game Over!");
-			RestartGame();
 		}
+		// if (ui.CheckWin()){
+		// 	alert("You won!");
+		// }
 	}
 	else{
 		tc = "F";
-		if(gameBoard.nmines_array[y][x]<20)
+		if(ui.n_array[x][y]<20)
 		{
-			gameBoard.nmines_array[y][x] += 20;
+			ui.n_array[x][y] += 20;
 			gfx.component(x*50,y*50,tc);
 		}
 		else{
-			gameBoard.nmines_array[y][x] -= 20;
+			ui.n_array[x][y] -= 20;
 			gfx.context.clearRect(x*50,y*50,50,50);
 			gfx.component(x*50,y*50);
 			// clean the spot
 		}
 	}
-	if (CheckWin()==true){
+	if (checkComplete()==true){
 		alert("You are win!");
-		RestartGame();
 	}
+
+}
+function RestartGame(){
+	history.go(0);
+}
+function quitgame(){
+	window.close();
 }
 
-function WhenChick_0(y,x){
-	if(gameBoard.nmines_array[y][x]==0){
-		tc =  "" + gameBoard.nmines_array[y][x];
-		gfx.component(x*50,y*50,"");
-		gfx.component(x*50,y*50,tc);
-		gameBoard.nmines_array[y][x] = gameBoard.nmines_array[y][x]+10;
-		for (let i =y-1; i <= y+1; i++){
-			for (let j = x-1; j <= x+1; j++){
-				if (i>=0&&i<9&&j>=0&&j<9){				
-					WhenChick_0(i,j);
-				}
-			}
-		}
-	}
-	else if(gameBoard.nmines_array[y][x]<=8){
-		tc =  "" + gameBoard.nmines_array[y][x];
-		gfx.component(x*50,y*50,"");
-		gfx.component(x*50,y*50,tc);
-		gameBoard.nmines_array[y][x] = gameBoard.nmines_array[y][x]+10;
-	}
-}
-
-function CheckWin(){
+function checkComplete(){
 	let over = true;
 	let n_mine_flag = 0;
 	for (let i = 0; i < gameBoard.width; i++){
 		for (let j = 0; j < gameBoard.height; j++){
-			if (gameBoard.nmines_array[i][j] == 29){
+			if (ui.n_array[i][j] == 29){
 				n_mine_flag++;
 			}
-			if (gameBoard.nmines_array[i][j] >= 20 && gameBoard.nmines_array[i][j] < 29){
+			if (ui.n_array[i][j] >= 20 && ui.n_array[i][j] < 29){
 				return false;
 			}
 		}
@@ -233,19 +285,5 @@ function CheckWin(){
 	if (n_mine_flag == gameBoard.mines)
 	{
 		return over;
-	}
-}
-
-function RestartGame(){
-	let choice=prompt("Do you want to play again? y/n")
-	if(choice =="y" ){
-		history.go(0);
-	}
-	else if(choice =="n" ){
-		window.close();
-	}
-	else{
-		alert("wrong choice");
-		history.go(0);
 	}
 }
